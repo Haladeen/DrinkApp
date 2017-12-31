685f19b75ca325b52953eb5462a45a45f0a30da8
Haladeen <nathan.rossler@skynet.be>
2017-12-31 01:07:35 +0100

    Design acceuil liste

--- app/src/main/java/com/example/natha/drinkapp/Cartes.java
+++ app/src/main/java/com/example/natha/drinkapp/Cartes.java
@@ -6,13 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.GridLayout;
 
 
-public class Liste extends Fragment {
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Cartes extends Fragment {
 
-    public Liste() {
+
+    public Cartes() {
         // Required empty public constructor
     }
 
@@ -21,8 +23,7 @@ public class Liste extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.liste_layout, container, false);
-
+        return inflater.inflate(R.layout.cartes_layout, container, false);
     }
 
 }
